0e28490bd26bcbd97f841a3ddfe5f74914d19862
Lucas Overbey <lucasaoverbey@gmail.com>
2022-04-07 18:31:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+//Event Listeners
+todoButton.addEventListener('click', addToDo);
+//Functions
+
+function addToDo(event){
+    event.preventDefault(); ///prevent form from refreshing
+    //ToDoDIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //createLI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //completedbutton
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //deletedbutton
+    const deletedButton = document.createElement('button');
+    deletedButton.innerHTML = '<i class="fas fa-trash"></i>'
+    deletedButton.classList.add("delete-btn");
+    todoDiv.appendChild(deletedButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+    //Clear text box
+    todoInput.value = "";
+
+}
